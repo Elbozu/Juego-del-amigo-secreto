1c5ed34e475467cc7a5fe4c248bd14cc3fb66f29
Elbozu <sandyjr8@gmail.com>
2025-02-03 17:46:01 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+let nombres = [];
+
+function agregarNombre() {
+    const input = document.getElementById('nombre');
+    const nombre = input.value.trim();
+    if (nombre !== "" && !nombres.includes(nombre)) {
+        nombres.push(nombre);
+        actualizarLista();
+        input.value = "";
+    }
+}
+
+function actualizarLista() {
+    const lista = document.getElementById('listaNombres');
+    lista.innerHTML = "";
+    nombres.forEach(nombre => {
+        const li = document.createElement('li');
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (nombres.length === 0) {
+        alert("Debes agregar los nombres antes de jugar");
+        return;
+    }
+    if (nombres.length > 1) {
+        const indice = Math.floor(Math.random() * nombres.length);
+        document.getElementById('resultado').textContent = `El amigo secreto es: ${nombres[indice]}`;
+    } else {
+        document.getElementById('resultado').textContent = "Agrega al menos dos nombres para sortear.";
+    }
+}
+
+function reiniciarJuego() {
+    nombres = [];
+    actualizarLista();
+    document.getElementById('resultado').textContent = "";
+}
